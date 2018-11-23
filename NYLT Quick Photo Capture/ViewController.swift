080a36c81661307cb3546a3e4a3670042ad2d2f7
Aroon Narayanan <aroonnarayanan@gmail.com>
2018-11-23 15:35:44 -0500

    Initial Commit

---
+++ NYLT Quick Photo Capture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYLT Quick Photo Capture
+//
+//  Created by Aroon Narayanan on 11/23/18.
+//  Copyright © 2018 Atlanta Area Council NYLT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
