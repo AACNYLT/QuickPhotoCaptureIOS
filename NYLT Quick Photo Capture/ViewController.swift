c41eb59bb092ba5708a3acde74022ab0f197f235
Aroon Narayanan <aroon@gatech.edu>
2018-11-25 17:56:53 -0500

    Added cocoapods, JSON file import, search, and refactored table view

--- NYLT Quick Photo Capture/ViewController.swift
+++ NYLT Quick Photo Capture/ViewController.swift
@@ -5,21 +5,22 @@
 //  Created by Aroon Narayanan on 11/23/18.
 //  Copyright © 2018 Atlanta Area Council NYLT. All rights reserved.
 //
-
+ 
 import UIKit
+import MobileCoreServices
+import Zip
 
-class ScoutTableViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ScoutTableViewController: UITableViewController, UINavigationControllerDelegate {
     
     // MARK: Data Source
     var scouts: [Scout] = []
+    var filteredScouts = [Scout]()
     func getScouts() {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         CampHubScouts().get(withCompletion: {(scoutResults: [Scout]?) -> Void in
             UIApplication.shared.isNetworkActivityIndicatorVisible = false
             if (scoutResults == nil) {
-                let alert = UIAlertController(title: "Error", message: "We weren't able to load scouts - make sure you have an internet connection.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
-                self.present(alert, animated: true)
+                self.Notify(message: "We weren't able to load scouts - make sure you have an internet connection.", title: "Error")
             } else {
                 self.scouts = scoutResults!
                 self.tableView.reloadData()
@@ -30,34 +31,111 @@ class ScoutTableViewController: UITableViewController, UINavigationControllerDel
     var imagePicker: UIImagePickerController!
     var selectedScout: Scout!
     
-    // MARK: Overrides
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
-        tableView.tableFooterView = UIView()
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Filter Scouts"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return scouts.count
+    // MARK: Search stuff
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        
-        cell.textLabel?.text = scouts[indexPath.row].FirstName + " " + scouts[indexPath.row].LastName
-        
-        return cell
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let scout = scouts[indexPath.row];
-        takePhoto(scout: scout)
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredScouts = scouts.filter({( scout : Scout) -> Bool in
+            return scout.FirstName.lowercased().contains(searchText.lowercased()) || scout.LastName.lowercased().contains(searchText.lowercased())
+        })
+        tableView.reloadData()
     }
     
+    // MARK: Image stuff
+    func takePhoto(scout: Scout) {
+        selectedScout = scout;
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: document stuff
+    func openJson() {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeJSON)], in: .import)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        self.present(documentPicker, animated: true)
+    }
+    
+    // MARK: menus
+    @IBAction func showExportMenu(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Export", message: "Export the photos you've taken online or locally in a file.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Save to File", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Upload to CampHub", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.popoverPresentationController?.barButtonItem = sender
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func showImportMenu(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Load Scouts", message: "Populate the app with scouts. NOTE: this will replace any scouts already in the app.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Open File", style: .default, handler: {(action: UIAlertAction) -> Void in
+            self.openJson()
+        }))
+        alert.addAction(UIAlertAction(title: "Download from CampHub", style: .default, handler:{(action: UIAlertAction) -> Void in
+            self.getScouts()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.popoverPresentationController?.barButtonItem = sender
+        self.present(alert, animated: true)
+    }
+    
+    // MARK: general utils
+    func Notify(message: String, title: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+}
+
+// MARK: document stuff
+extension ScoutTableViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let fileContents = try? String(contentsOf: urls.first!) else {
+            self.Notify(message: "We couldn't read the file you selected.", title: "Error")
+            return
+        }
+        let data = fileContents.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        //        guard let scoutResults = try? decoder.decode([Scout].self, from: data) else {
+        //            self.Notify(message: "We couldn't decode the file you selected.", title: "Error")
+        //            return
+        //        }
+        var scoutResults: [Scout] = []
+        do {
+            try scoutResults = decoder.decode([Scout].self, from: data)
+        } catch {
+            self.Notify(message: "We couldn't decode the file you selected.", title: "Error")
+            print(error)
+            return
+        }
+        self.scouts = scoutResults
+        self.tableView.reloadData()
+    }
+}
+
+// MARK: image stuff
+extension ScoutTableViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         imagePicker.dismiss(animated: true, completion: nil)
         let fileManager = FileManager.default
@@ -69,43 +147,48 @@ class ScoutTableViewController: UITableViewController, UINavigationControllerDel
             return
         }
     }
-    
-    // MARK: photo stuff
-    func takePhoto(scout: Scout) {
-        selectedScout = scout;
-        imagePicker =  UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .camera
-        imagePicker.allowsEditing = true
-        present(imagePicker, animated: true, completion: nil)
+}
+
+// MARK: table stuff
+extension ScoutTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-    // MARK: menus
     
-    @IBAction func showExportMenu(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Export", message: "Export the photos you've taken online or locally in a file.", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Local File", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Upload to CampHub", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        alert.popoverPresentationController?.barButtonItem = sender
-        self.present(alert, animated: true)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltering() ? filteredScouts.count : scouts.count
     }
     
-    @IBAction func showImportMenu(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Load Scouts", message: "Populate the app with scouts.", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Local File", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Download from CampHub", style: .default, handler:{(action: UIAlertAction) -> Void in
-            self.getScouts()
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        alert.popoverPresentationController?.barButtonItem = sender
-        self.present(alert, animated: true)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        let scout = isFiltering() ? filteredScouts[indexPath.row] : scouts[indexPath.row]
+        let scoutUnit = scout.CourseID != nil ? scout.CourseID! % 2 == 0 ? "Blue" : "Red" : nil
+        cell.textLabel?.text = scout.FirstName + " " + scout.LastName
+        cell.detailTextLabel?.text = scout.Team != nil && scoutUnit != nil ? scoutUnit! + " - " + scout.Team! : nil
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let scout = isFiltering() ? filteredScouts[indexPath.row] : scouts[indexPath.row];
+        takePhoto(scout: scout)
     }
 }
+
+// MARK: search stuff
+extension ScoutTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
 
 struct Scout: Codable {
     var ScoutID: Int
     var FirstName: String
     var LastName: String
+    var Team: String?
+    var CourseID: Int?
 }
 
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'NYLT Quick Photo Capture' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NYLT Quick Photo Capture
+  source 'https://github.com/CocoaPods/Specs.git'
+  pod 'Zip', '~> 1.1'
+
+  target 'NYLT Quick Photo CaptureTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
